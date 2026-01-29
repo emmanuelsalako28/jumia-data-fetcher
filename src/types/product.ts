d844d4254878104317dfb0ea3f4f12a11240eb94
emmanuelsalako28 <emmanuelsalako28@gmail.com>
2026-01-29 11:40:40 +0100

    update

--- src/types/product.ts
+++ src/types/product.ts
@@ -20,13 +20,4 @@ export interface ProductBrief extends ProductData {
 
 export const JUMIA_COUNTRIES: JumiaCountry[] = [
   { code: "NG", name: "Nigeria", domain: ".com.ng" },
-  { code: "DZ", name: "Algeria", domain: ".dz" },
-  { code: "EG", name: "Egypt", domain: ".com.eg" },
-  { code: "GH", name: "Ghana", domain: ".com.gh" },
-  { code: "CI", name: "Ivory Coast", domain: ".ci" },
-  { code: "KE", name: "Kenya", domain: ".co.ke" },
-  { code: "MA", name: "Morocco", domain: ".ma" },
-  { code: "SN", name: "Senegal", domain: ".sn" },
-  { code: "TN", name: "Tunisia", domain: ".com.tn" },
-  { code: "UG", name: "Uganda", domain: ".ug" },
 ];
